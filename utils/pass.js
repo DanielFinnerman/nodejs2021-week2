b266f6fb7bc0daf619b278807d1929297d97fd21
Daniel Finnerman <danu95d@hotmail.com>
2021-03-18 12:07:34 +0200

    json web token

--- utils/pass.js
+++ utils/pass.js
@@ -1,11 +1,15 @@
 const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 const {getUserLogin} = require('../models/userModel');
 
 passport.use(
     new Strategy((username, password, done) => {
         // get user by username from getUserLogin
         const user = getUserLogin(username);
+        console.log(user);
         // if user is undefined
         if (user === undefined) {
             return done(null, false);
@@ -15,8 +19,26 @@ passport.use(
             return done(null, false);
         }
         // if all is ok
-        return done(null, user.id);
+        delete user.password;
+        return done(null, user);
     })
+);
+
+passport.use(
+    new JWTStrategy(
+        {
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey: '1234',
+        },
+        (jwtPayload, done) => {
+            console.log('payload: ', jwtPayload);
+            const user = getUserLogin(jwtPayload.email);
+            if (user === undefined) {
+                return done(null, false);
+            }
+            return done(null, jwtPayload);
+        }
+    )
 );
 
 module.exports = passport;
